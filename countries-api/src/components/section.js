236131a53b1bbb9e16f0f0085540808919c8314c
martin gardner II <martingardnerii@gmail.com>
2019-11-26 17:24:11 -0600

    add in filter search and make it work with search term

--- countries-api/src/components/section.js
+++ countries-api/src/components/section.js
@@ -8,8 +8,9 @@ import CountryDrilldown from "./country-drilldown";
 const Section = props => {
   const [getDrilldownVal, setDrilldownVal] = useState(null);
   const [getFilterWord, setFilterWord] = useState("");
-  const [getFilterCategory, setFilterCategory] = useState("");
+  const [getFilterCategory, setFilterCategory] = useState("none");
   const [getFilterData, setFilterData] = useState([]);
+  const [getFilterDropdownList, setFilterDropdownList] = useState([]);
 
   const drilldown = country3Code => {
     let index = props.data.findIndex(arr => arr.alpha3Code === country3Code);
@@ -21,27 +22,45 @@ const Section = props => {
   };
 
   const updateFilterWord = e => setFilterWord(e.target.value);
+  const updateFilterCategory = e => setFilterCategory(e.target.value);
 
-  /*
-    let filterwordarr = props.data.filter(el => {
-      return el.name === e.target.value;
-    });
-    console.log("filterwordarr", filterwordarr);
-    */
-  /* update Filter Data if updateFilterWord or FilterCategory  */
-
+  /* matches search country if name matches word */
   let filterWordArr = () => {
     return props.data.filter(el => {
       return el.name === getFilterWord;
     });
   };
 
+  /* matches filter Category if part of it */
+  let filterRegionArr = () => {
+    return props.data.filter(ra => {
+      return ra.region === getFilterCategory;
+    });
+  };
+
+  /*  create filterDropdown List */
+  useEffect(() => {
+    let filterSet = new Set();
+    props.data.filter(e => {
+      filterSet.add(e.region);
+    });
+    let filterCatArray = [...filterSet].sort();
+
+    setFilterDropdownList(filterCatArray);
+  }, []);
+
+  /* update Filter Data if updateFilterWord or FilterCategory  */
   useEffect(() => {
     let wordArr = filterWordArr();
+    let filterArr = filterRegionArr();
     console.log("wordArr", wordArr);
+    console.log("filterArr", filterArr);
 
-    setFilterData(wordArr);
-  }, [getFilterWord]);
+    let filterSet = [...new Set([...wordArr, ...filterArr])];
+    console.log("filterSet", filterSet);
+    console.log("spreadset", [...filterSet]);
+    setFilterData([...filterSet]);
+  }, [getFilterWord, getFilterCategory]);
 
   let countryData = getFilterData.length > 0 ? getFilterData : props.data;
 
@@ -63,7 +82,20 @@ const Section = props => {
                 onChange={updateFilterWord}
               />
             </Col>
-            <Col></Col>
+            <Col>
+              <select onChange={updateFilterCategory} value={getFilterCategory}>
+                <option value="none" key="none">
+                  none
+                </option>
+                {getFilterDropdownList.map(reg => {
+                  return (
+                    <option value={reg} key={reg}>
+                      {reg}
+                    </option>
+                  );
+                })}
+              </select>
+            </Col>
           </Row>
           <Row>
             {countryData.map(val => (
