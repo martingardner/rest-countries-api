189317eaf5ef3f724c8970bb6e3f34cbb76a0b6e
martin gardner II <martingardnerii@gmail.com>
2019-11-26 16:17:02 -0600

    add in search for country field and logic

--- countries-api/src/components/section.js
+++ countries-api/src/components/section.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -9,8 +9,7 @@ const Section = props => {
   const [getDrilldownVal, setDrilldownVal] = useState(null);
   const [getFilterWord, setFilterWord] = useState("");
   const [getFilterCategory, setFilterCategory] = useState("");
-
-  let data = props.data;
+  const [getFilterData, setFilterData] = useState([]);
 
   const drilldown = country3Code => {
     let index = props.data.findIndex(arr => arr.alpha3Code === country3Code);
@@ -21,6 +20,31 @@ const Section = props => {
     setDrilldownVal(null);
   };
 
+  const updateFilterWord = e => setFilterWord(e.target.value);
+
+  /*
+    let filterwordarr = props.data.filter(el => {
+      return el.name === e.target.value;
+    });
+    console.log("filterwordarr", filterwordarr);
+    */
+  /* update Filter Data if updateFilterWord or FilterCategory  */
+
+  let filterWordArr = () => {
+    return props.data.filter(el => {
+      return el.name === getFilterWord;
+    });
+  };
+
+  useEffect(() => {
+    let wordArr = filterWordArr();
+    console.log("wordArr", wordArr);
+
+    setFilterData(wordArr);
+  }, [getFilterWord]);
+
+  let countryData = getFilterData.length > 0 ? getFilterData : props.data;
+
   return (
     <section>
       {getDrilldownVal ? (
@@ -30,9 +54,19 @@ const Section = props => {
         />
       ) : (
         <>
-          <Row></Row>
           <Row>
-            {data.map(val => (
+            <Col>
+              <input
+                type="text"
+                placeholder="search term"
+                value={getFilterWord}
+                onChange={updateFilterWord}
+              />
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row>
+            {countryData.map(val => (
               <Country data={val} drilldown={drilldown} key={val.name} />
             ))}
           </Row>
