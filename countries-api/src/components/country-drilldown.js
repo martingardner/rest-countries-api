80522bf1e5466084c1c0dd75e1ac098964c9d90a
martin gardner II <martingardnerii@gmail.com>
2019-11-27 13:24:51 -0600

    added in array formatter for country-drilldown

--- countries-api/src/components/country-drilldown.js
+++ countries-api/src/components/country-drilldown.js
@@ -6,6 +6,25 @@ import Col from "react-bootstrap/Col";
 import "../css/country-drilldown.css";
 
 const CountryDrilldown = props => {
+  const multiArrayComma = (arr, objparam) => {
+    if (Array.isArray(arr)) {
+      let arrLen = arr.length;
+      let arrString = "";
+      for (var a = 0; a < arrLen; a++) {
+        if (objparam) {
+          arrString += arr[a][objparam];
+        } else {
+          arrString += arr[a];
+        }
+        if (a < arrLen - 1) {
+          arrString += " , ";
+        }
+      }
+
+      return arrString;
+    }
+  };
+
   return (
     <div className="country-drilldown">
       <Row>
@@ -55,26 +74,28 @@ const CountryDrilldown = props => {
               <div>
                 <span>
                   <b>Top Level Domain: </b>
-                  {props.drilldata.topLevelDomain.map(val => {
+                  {multiArrayComma(props.drilldata.topLevelDomain)}
+                  {/*props.drilldata.topLevelDomain.map(val => {
                     return `${val} , `;
-                  })}
+                  })*/}
                 </span>
               </div>
               <div>
                 <span>
                   <b>Currencies: </b>
-                  {props.drilldata.currencies.map(money => {
+                  {multiArrayComma(props.drilldata.currencies, "name")}
+                  {/*props.drilldata.currencies.map(money => {
                     return `${money.name} , `;
-                  })}
+                  })*/}
                 </span>
               </div>
               <div>
                 <span>
                   <b>Languages: </b>
-
-                  {props.drilldata.languages.map(lang => {
+                  {multiArrayComma(props.drilldata.languages, "name")}
+                  {/*props.drilldata.languages.map(lang => {
                     return `${lang.name} , `;
-                  })}
+                  })*/}
                 </span>
               </div>
             </Col>
